cda76dccd16c0fe6663aea0b30a9104ab9a1fdad
tmarcorf <tmarco783@gmail.com>
2025-07-17 00:07:51 -0300

    feat: added the first entities

---
+++ src/ZenMoney/ZenMoney.Core/Entities/Income.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenMoney.Core.Enums;
+
+namespace ZenMoney.Core.Entities
+{
+    public class Income : BaseEntity
+    {
+        [Required]
+        public IncomeTypeEnum Type { get; set; }
+
+        [Required]
+        public DateOnly Date { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ src/ZenMoney/ZenMoney.Core/Enums/ExpenseTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenMoney.Core.Enums
+{
+    public enum ExpenseTypeEnum
+    {
+        FIXED = 0,
+        VARIABLE = 1,
+    }
+}
