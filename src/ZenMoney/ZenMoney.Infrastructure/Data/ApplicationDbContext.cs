2ac7cf50468428ce9b0f5d3c600909d55152c7bc
tmarcorf <tmarco783@gmail.com>
2025-07-22 00:39:57 -0300

    feat: added infrastructure configuration

---
+++ src/ZenMoney/ZenMoney.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ZenMoney.Core.Entities;
+
+namespace ZenMoney.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Income> Incomes { get; set; }
+
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ src/ZenMoney/ZenMoney.Infrastructure/IoC/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ZenMoney.Core.Entities;
+using ZenMoney.Infrastructure.Data;
+
+namespace ZenMoney.Infrastructure.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, b => b.MigrationsAssembly("ZenMoney.Infrastructure"));
+            });
+
+            services.AddIdentity<User, IdentityRole<Guid>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
+            });
+
+            // services goes here
+
+            return services;
+        }
+    }
+}
