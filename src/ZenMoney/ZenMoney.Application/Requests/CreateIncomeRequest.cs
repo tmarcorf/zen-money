5a5d7e5f16906394149dee36be1a19d3429f24ea
tmarcorf <tmarco783@gmail.com>
2025-07-24 01:23:55 -0300

    feat: added Income Service to application layer

---
+++ src/ZenMoney/ZenMoney.Application/Requests/CreateIncomeRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenMoney.Core.Enums;
+
+namespace ZenMoney.Application.Requests
+{
+    public class CreateIncomeRequest
+    {
+        public IncomeTypeEnum Type { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
