98dfa5f00da37c1dfa52b58e5b6f5063f58f4e6d
tmarcorf <tmarco783@gmail.com>
2025-08-10 01:45:32 -0300

    chore: updated to Angular 18

--- src/WebApp/ZenMoneyApp/src/app/interceptors/loading.interceptor.ts
+++ src/WebApp/ZenMoneyApp/src/app/interceptors/loading.interceptor.ts
@@ -1,11 +1,6 @@
 // loading.interceptor.ts
 import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent
-} from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { LoadingService } from '../services/loading.service';
